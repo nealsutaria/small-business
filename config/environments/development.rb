b70f6a52f9e1df320024f036d9d5e522bdc21605
neal-sutaria <97972270+neal-sutaria@users.noreply.github.com>
2025-06-17 11:28:17 -0500

    Added forgot password email logic

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,17 @@ Rails.application.configure do
   config.action_controller.raise_on_missing_callback_actions = true
 
   config.active_storage.service = :cloudinary
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: Rails.application.credentials.dig(:gmail, :user),
+    password: Rails.application.credentials.dig(:gmail, :password),
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
 end
